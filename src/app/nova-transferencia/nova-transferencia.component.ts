6decbfbb5c3e60947644002a7bbc1cf918fc076b
Wesley Claudino <claudinowesley11@gmail.com>
2022-01-26 19:00:11 -0300

    new transfer form

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,10 +1,17 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'nova-transferencia',
-    templateUrl: './nova-transferencia.component.html',
-    styleUrls: ['./nova-transferencia.component.scss']
+  selector: 'nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
-export class NovaTransferenciaComponent{
+export class NovaTransferenciaComponent {
 
+  valor!: number;
+  destino!: number;
+
+  transferir() {
+    console.log('Valor: ', this.valor);
+    console.log('Destino: ', this.destino);
+  }
 }
