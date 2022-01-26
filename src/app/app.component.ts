b9eab0e4b386a1d6acd3155409211162f17a2de9
Wesley Claudino <claudinowesley11@gmail.com>
2022-01-26 19:33:20 -0300

    exporting component data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bytebank-angular';
+  transferencia: any;
+
+  transferir($event: any) {
+    console.log($event);
+    this.transferencia = $event;
+  }
 }
--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'nova-transferencia',
@@ -7,11 +8,22 @@ import { Component } from '@angular/core';
 })
 export class NovaTransferenciaComponent {
 
+  @Output() aoTransferir = new EventEmitter<any>();
+
   valor!: number;
   destino!: number;
 
   transferir() {
-    console.log('Valor: ', this.valor);
-    console.log('Destino: ', this.destino);
+    const valorEmitir = {
+      valor: this.valor,
+      destino: this.destino
+    };
+    this.aoTransferir.emit(valorEmitir);
+    this.limparCampos();
+  }
+
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
   }
 }
