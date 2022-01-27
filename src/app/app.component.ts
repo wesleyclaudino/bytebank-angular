3cbe09d35a15dfaf67ae0b25e86b1eb42dc1a62b
Wesley Claudino <claudinowesley11@gmail.com>
2022-01-27 17:28:56 -0300

    transfers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bytebank-angular';
-  transferencia: any;
+  transferencias: any[] = [];
 
   transferir($event: any) {
-    console.log($event);
-    this.transferencia = $event;
+    const transferencia = {...$event, data: new Date()};
+    this.transferencias.push(transferencia);
   }
 }
